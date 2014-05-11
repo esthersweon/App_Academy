94ce08e21aa7c853f0449ff31cc07de21bcb124f
Esther S Weon <esther.s.weon@gmail.com>
2014-05-11 15:41:43 -0700

    revise sum_with_myBind

--- W6/W6D3/sum_with_myBind.js
+++ W6/W6D3/sum_with_myBind.js
@@ -13,17 +13,19 @@ Function.prototype.myBind = function(object) {
 
   return function () {
     var otherArgs = Array.prototype.slice.call(arguments);
-    f.apply(object, args.concat(otherArgs));
+    return f.apply(object, args.concat(otherArgs));
   }
 };
 
+
+
+//to test the myBind function on an unrelated object
 
 function myObject(name) {
   this.name = name;
 };
 
 var mySum = function() {
-  console.log(this.name);
   var sum = 0;
   for(var i = 0; i < arguments.length; i++) {
     sum += arguments[i];
@@ -32,6 +34,9 @@ var mySum = function() {
 };
 
 var bob = new myObject("bob");
+
 //bob.mySum(1, 2);
 var myBoundFunction = mySum.myBind(bob, 1, 2);
+
+//bob.mySum(1, 2, 3);
 myBoundFunction(3);
