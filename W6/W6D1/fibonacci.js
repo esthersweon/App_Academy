2511392dda8858ef5bc3b3040ec2f8448e7be089
Esther S Weon <esther.s.weon@gmail.com>
2014-05-12 03:02:44 -0700

    deleted ruby file

--- W6/W6D1/fibonacci.js
+++ W6/W6D1/fibonacci.js
@@ -5,8 +5,6 @@ var fibonacci = function(n){
     return [0, 1];
   } else {
     var last = fibonacci(n - 1);
-    last.push(last[last.length - 2] + last[last.length - 1]);
-    return last;
+    return last.concat(last[last.length - 2] + last[last.length - 1]);
   }
-
 }
